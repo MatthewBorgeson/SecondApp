c313e7cc91530e81f8b699517f3d16461dbeefaa
Matthias Wolfgang <hibridmatthias@sbcglobal.net>
2011-07-02 08:10:19 -0500

    Inital Commit for SecondApp baby

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondApp::Application.config.secret_token = '6a6d792c035ef7a315b0536f4639a4359760b8278f3101a4e4006dd0cb03a1c7c44a4eb29ed37ab7c35ffa50cee1f879790614479d1843735236d9ad6db4bb9e'
